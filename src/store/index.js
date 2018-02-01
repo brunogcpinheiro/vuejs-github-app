adccc2789bbd973fd60e2c32db2be9bdfde914da
brunogcarpinellip <brunogcarpinellip@gmail.com>
2018-02-01 12:21:57 -0500

    Creating the mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
     user: 'brunogcpinheiro'
-  }
+  },
+  mutations
 })
 
 export default store
